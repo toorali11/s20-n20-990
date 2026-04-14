cb6057fd71c3f9dcc117986bf8fd5990249cc9a0
Toor Ali <toorali11@gmail.com>
2026-04-14 22:01:12 +0200

    added the manual hooks for ksu

--- fs/exec.c
+++ fs/exec.c
@@ -1912,6 +1912,12 @@ out_ret:
 	return retval;
 }
 
+#ifdef CONFIG_KSU
+__attribute__((hot))
+extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr,
+				void *argv, void *envp, int *flags);
+#endif
+
 static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr argv,
 			      struct user_arg_ptr envp,
@@ -1934,6 +1940,9 @@ int do_execve(struct filename *filename,
 {
 	struct user_arg_ptr argv = { .ptr.native = __argv };
 	struct user_arg_ptr envp = { .ptr.native = __envp };
+#ifdef CONFIG_KSU
+	ksu_handle_execveat((int *)AT_FDCWD, &filename, &argv, &envp, 0);
+#endif	
 	return do_execveat_common(AT_FDCWD, filename, argv, envp, 0);
 }
 
