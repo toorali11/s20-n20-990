6c90a6bc09441b0ad9ba1503a27ed40753942a07
Toor Ali <toorali11@gmail.com>
2026-04-15 16:04:36 +0200

    change version and script

--- build.sh
+++ build.sh
@@ -236,9 +236,9 @@ if [ -z "$RECOVERY" ] && [ -z "$DTBS" ]; then
     DATE=`date +"%d-%m-%Y_%H-%M-%S"`
 
     if [[ "$KSU_OPTION" == "y" ]]; then
-        NAME="$version"_"$MODEL"_UNOFFICIAL_KSU_"$DATE".zip
+        NAME="$MODEL"_"$version"_KSU_"$DATE".zip
     else
-        NAME="$version"_"$MODEL"_UNOFFICIAL_"$DATE".zip
+        NAME="$MODEL"_"$version"_"$DATE".zip
     fi
     zip -r -qq ../"$NAME" .
     popd > /dev/null
